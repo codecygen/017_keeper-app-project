a6657ce8a435484bbe79c3cf3d04b276c862189b
gingercook <vahitaras@gmail.com>
2021-09-22 21:07:53 -0400

    Main parts added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Note from './components/Note';
 import Footer from './components/Footer';
 
 const App = () => {
     return(
         <div>
             <Header />
+            <Note />
             <Footer />
         </div>
     );
