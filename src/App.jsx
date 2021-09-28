4bf862f9183ed1bf2d278e105be442f9283b7bdd
gingercook <vahitaras@gmail.com>
2021-09-27 22:09:42 -0400

    notes.js added and all files are combined!

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import React from 'react';
 import Header from './components/Header';
 import Note from './components/Note';
 import Footer from './components/Footer';
+import notes from './notes';
+
+const createCard = (note, index) => {
+    return(
+        <Note 
+            key={index}
+            {...notes[index]}
+        />
+    );
+}
 
 const App = () => {
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map((note, index) => 
+                    <Note 
+                        key={index}
+                        {...notes[index]}
+                    />
+            )}
+            {/* 
+            <Note {...notes[0]} />
+            <Note {...notes[1]} />
+            <Note {...notes[2]} />
+            <Note {...notes[3]} /> */}
+
+            {/* <Note 
+                title={notes.title}
+                content={notes.content}
+            /> */}
+
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Note = () => {
+const Note = (props) => {
     return(
         <section className="note">
-            <h1>This is the note title</h1>
-            <p>This is the note content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </section>
     );
 }
